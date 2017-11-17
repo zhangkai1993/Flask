cfddcfe25a14f3431be2287bfe597eb2048a5ec0
Kai Zhang <kai.zhang@unhackablecloud.com>
2017-11-17 10:48:55 -0800

    Fixed name_error issue

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from config import DevConfig
 
-app = Flask(_name_)
+app = Flask(__name__)
 app.config.from_object(DevConfig)
 
 @app.route('/')
 def home():
         return '<h1>Hello World!</h1>'
 
-    if _name_ == '_main_': 
+if __name__ == '__main__': 
         app.run()
