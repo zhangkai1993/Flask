9d240be675975daea5f1151db40e49d069d72a2b
Kai Zhang <kai.zhang@unhackablecloud.com>
2017-11-17 18:08:43 -0800

    oct17

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from config import DevConfig
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func
 
 app = Flask(__name__)
 app.config.from_object(DevConfig)
 db = SQLAlchemy(app)
 
 class User(db.Model):
-    __tablename__ = 'user'
 
     id = db.Column(db.Integer(),primary_key=True)
     username = db.Column(db.String(255))
@@ -21,6 +21,12 @@ class User(db.Model):
         self.username = username
     def __repr__(self):
         return "<User '{}'>".format(self.username)
+
+
+tags = db.Table('post_tags',
+        db.Column('post_id',db.Integer,db.ForeignKey('post.id')),
+        db.Column('tag_id',db.Integer,db.ForeignKey('tag.id'))
+        )
 
 class Post(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
@@ -36,7 +42,7 @@ class Post(db.Model):
     tags = db.relationship(
                 'Tag',
                 secondary = tags,
-                backref = db.backref('posts',lazy = 'dynamic')
+                backref = db.backref('posts',lazy='dynamic')
             )
     user_id = db.Column(db.Integer(),db.ForeignKey('user.id'))
 
@@ -44,6 +50,8 @@ class Post(db.Model):
         self.title = title
     def __repr__(self):
         return "<Post '{}'>".format(self.title)
+
+
 
 class Comment(db.Model):
     id = db.Column(db.Integer(),primary_key=True)
@@ -53,7 +61,7 @@ class Comment(db.Model):
     post_id = db.Column(db.Integer(), db.ForeignKey('post.id'))
 
     def __repr__(self):
-        return "<Comment '{}'>".format(selft.text[:15])
+        return "<Comment '{}'>".format(self.text[:15])
 
 class Tag(db.Model):
     id = db.Column(db.Integer(), primary_key = True)
@@ -62,11 +70,68 @@ class Tag(db.Model):
     def __init__(self,title):
         self.title = title
     def __repr__(self):
-        return "<Tag '{}'>".format(selft.title)
+        return "<Tag '{}'>".format(self.title)
+
+
+def sidebar_data():
+    recent = Post.query.order_by(Post.publish_data.desc()).limit(5).all()
+    top_tags  = db.session.query(Tag,func.count(tags.c.post_id).label('total')).join(tags).group_by(Tag).order_by('total DESC').limit(5).all()
+
+    return recent,tog_tags
 
 @app.route('/')
-def home():
-        return '<h1>Hello World!</h1>'
+@app.route('/<int:page>')
+def home(page=1):
+    posts = Post.query.order_by(Post.publish_data.desc()).paginate(page,10)
+    recent,top_tags = sidebar_data()
 
+    return render_templates(
+        'home.html',
+        posts = posts,
+        recent = recent,
+        top_tags = top_tags
+        )
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    tags = post.tags
+    comments = post.comments.order_by(Comment.date.desc()).all()
+    recent,top_tags = sidebar_data()
+
+    return render_templats(
+    'post.html',
+     post = post,
+     tags = tags,
+     comments = comments,
+     recent = recent,
+     top_tags = top_tags
+    )
+
+@app.route('/tag/<string:tag_name>')
+def tag(tag_name):
+    tag = Tag.query.filter_by(title=tag_name).first_or_404()
+    posts = tag.posts.order_by(Post.publish_date.desc()).all()
+    recent,top_tags = sidebar_data()
+    
+    return render_template(
+        'tag.html',
+        tag = tag,
+        posts = posts,
+        recent = recent,
+        top_tags = top_tags
+    )
+
+@app.route('/user/<string:username>')
+def user(username):
+        user = User.query.filter_by(username=username).first_or_404()
+        posts = user.posts.order_by(Post.publish_data.desc()).all()
+        recent,top_tags = sidebar_data()
+        return render_template(
+                'user.html',
+                user = user,
+                posts=posts,
+                recent=recent,
+                top_tags=top_tags
+        )
 if __name__ == '__main__': 
         app.run()
